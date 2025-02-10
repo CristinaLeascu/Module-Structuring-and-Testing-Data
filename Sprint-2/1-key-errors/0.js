3ee2a8f748139d8d802e3b119eff9abdc6f2b1ad
Cristina Leascu <cristinaleascu@gmail.com>
2025-02-10 10:46:07 +0000

    Erased one variable declaration

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,15 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// there will be an error with the declaration of the variable str as is a parameter and declared on line 9 again.
 
-// call the function capitalise with a string input
+// call the function capitalize with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+function capitalize(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
-}
+} 
 
+console.log(capitalize('code'));
 // =============> write your explanation here
 // =============> write your new code here
